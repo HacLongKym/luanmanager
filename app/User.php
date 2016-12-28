51c424b1466b1485161aec360a345bf8396f3051
root <haclongkim9x@gmail.com>
2016-12-28 11:33:39 +0700

    update role for admin

--- app/User.php
+++ app/User.php
@@ -8,7 +8,27 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    const ROLE_ADMIN = 1;
+    const ROLE_MANAGER = 2;
+    const ROLE_USER = 4;
+    // public function roles()
+    // {
+    //     return $this->belongsToMany('App\Role', 'user_role', 'user_id', 'role_id');
+    // }
+    public function hasAnyRole($roles) {
+        if (is_array($roles)) {
+            foreach ($roles as  $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            return $this->hasRole($roles);
+        }
+    }
+    public function hasRole($role) {
+        return ($this->roles & $role);
+    }
     /**
      * The attributes that are mass assignable.
      *
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\User;
+// use App\Role;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +13,59 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // $this->createRoleUser();
+        $this->createUser();
     }
+    /**
+     * [createUser description]
+     * @return [type] [description]
+     */
+    public function createUser() {
+        // $role_user = Role::where('name', 'User')->first();
+        // $role_admin = Role::where('name', 'Admin')->first();
+        // $role_manager = Role::where('name', 'Manager')->first();
+
+        $user =  new User();
+        $user->name = 'user';
+        $user->email = 'user@gmail.com';
+        $user->role = User::ROLE_USER;
+        $user->password = bcrypt('123456');
+        $user->save();
+
+        $user_admin =  new User();
+        $user_admin->name = 'admin';
+        $user_admin->email = 'admin@gmail.com';
+        $user_admin->role = User::ROLE_ADMIN;
+        $user_admin->password = bcrypt('123456');
+        $user_admin->save();
+
+        $user_manager =  new User();
+        $user_manager->name = 'manager';
+        $user_manager->email = 'manager@gmail.com';
+        $user_manager->role = User::ROLE_MANAGER;
+        $user_manager->password = bcrypt('123456');
+        $user_manager->save();
+
+
+
+        // $user->roles()->attach($role_user);
+        // $user_admin->roles()->attach($role_admin);
+        // $user_manager->roles()->attach($role_manager);
+    }
+    // public function createRoleUser() {
+    //     $role_user = new Role();
+    //     $role_user->name = 'User';
+    //     $role_user->description = 'A normal user';
+    //     $role_user->save();
+
+    //     $role_admin = new Role();
+    //     $role_admin->name = 'Admin';
+    //     $role_admin->description = 'An Admin';
+    //     $role_admin->save();
+
+    //     $role_manager = new Role();
+    //     $role_manager->name = 'Manager';
+    //     $role_manager->description = 'A Manager';
+    //     $role_manager->save();
+    // }
 }
